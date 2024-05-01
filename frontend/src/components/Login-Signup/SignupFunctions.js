1d67568218dfade87d215d80c32e033b9bd3b48b
mohith <2200032200@kluniversity.in>
2024-05-01 17:03:42 +0530

    deploy ch

--- frontend/src/components/Login-Signup/SignupFunctions.js
+++ frontend/src/components/Login-Signup/SignupFunctions.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 export function registerUser(newUserDetails){
-    let apiUrl = 'http://localhost:8080/register'
+    let apiUrl = 'https://buus-booking-app-master1-4gc4-backend1.vercel.app/'
     return axios.post(apiUrl,newUserDetails,{
         headers:{
             'Content-Type': 'application/json'
--- frontend/src/components/routeSelector/routeApifunc.js
+++ frontend/src/components/routeSelector/routeApifunc.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 export async function getRoutesFromApi(startCity, destination) {
-    const baseURL = "http://localhost:8080/booking/"
+    const baseURL = "https://buus-booking-app-master1-4gc4-backend1.vercel.app//"
     let incoming = await axios.post(baseURL, { startCity, destination })
     return incoming
 }
